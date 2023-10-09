5ef43d6e7107dc17f6c123589a4e55264902bf7a
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-09 11:21:01 -0400

    first commit

---
+++ RangeSumOfBST938.java
@@ -0,0 +1,41 @@
+public class RangeSumOfBST938 {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+        TreeNode(int val) {this.val=val;}
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    static int sum = 0;
+    public static int rangeSumBST(TreeNode root, int low, int high){
+        if(root==null) return 0;
+        if(root.val>=low && root.val<=high){
+            sum+=root.val;
+        }
+        if(root.val>low){
+            rangeSumBST(root.left, low, high);
+        }
+        if(root.val<high){
+            rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode();
+        root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(15);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.right.right = new TreeNode(18);
+
+        int low = 7, high = 15;
+        System.out.print(rangeSumBST(root, low, high));
+    }
+}
