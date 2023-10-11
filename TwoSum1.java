571bc7a09a6213bb65c91ea4e3cefea32dc51fde
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-10 22:57:53 -0400

    Adding Two Sum Optimized solution

--- TwoSum1.java
+++ TwoSum1.java
@@ -1,4 +1,6 @@
+import java.util.*;
 public class TwoSum1 {
+    //BruteForce
     public static int[] twoSum(int[] nums, int target){
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j< nums.length;j++){
@@ -9,11 +11,26 @@ public class TwoSum1 {
         }
         return new int[]{};
     }
+    //Optimized
+    public static int[] twoSumOptimized(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
     public static void main(String[] args){
         int[] nums = {2,7,11,15};
         int target = 9;
         int[] result = twoSum(nums, target);
         for(int i=0;i<result.length;i++){
+            System.out.println(i);
+        }
+        int[] optimizedResult = twoSumOptimized(nums, target);
+        for(int i=0;i<optimizedResult.length;i++){
             System.out.println(i);
         }
     }
