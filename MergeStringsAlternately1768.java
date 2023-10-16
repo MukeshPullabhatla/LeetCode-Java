0faf3216ba2c5d3497c2ee76ce5d09bc6710d052
MukeshPullabhatla <amukeshps@gmail.com>
2023-10-15 21:08:04 -0400

    merge strings alternately add test case

--- MergeStringsAlternately1768.java
+++ MergeStringsAlternately1768.java
@@ -1,8 +1,7 @@
 public class MergeStringsAlternately1768 {
     public static String mergeAlternately(String word1, String word2){
-//        String word = "";
         StringBuilder word = new StringBuilder();
-        int i=0;
+        int i = 0;
         while(i<word1.length() || i<word2.length()){
             if(i<word1.length()){
                 word.append(word1.charAt(i));
@@ -12,12 +11,14 @@ public class MergeStringsAlternately1768 {
             }
             i++;
         }
-
         return word.toString();
     }
     public static void main(String[] args){
         String word1="abc";
         String word2="pqr";
+        String word3 = "ab";
+        String word4 = "pqrs";
         System.out.println(mergeAlternately(word1, word2));
+        System.out.println(mergeAlternately(word3, word4));
     }
 }
